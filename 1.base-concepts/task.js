c80bd56c2194d5a42dcb6f23af2dc29cb05373e8
Nassem <Nas7enova@gmail.com>
2026-04-18 11:10:32 +0500

    Первое задание выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
-"use strict"
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	const d = b ** 2 - 4 * a * c;
+	let arr = [];
+	if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+		arr.push(-b / (2 * a));
+		return arr;
+	} else {
+		const sqrtD = Math.sqrt(d);
+		arr.push((-b + sqrtD) / (2 * a));
+		arr.push((-b - sqrtD) / (2 * a));
+		return arr;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let rate = percent / 100 / 12;
+	let creditBody = amount - contribution;
+	const payment = creditBody * (rate + (rate / (((1 + rate) ** countMonths) - 1)));
+	if (creditBody <= 0) {
+		return 0;
+	} else {
+		const totalAmount = payment * countMonths;
+		return Math.round(totalAmount * 100) / 100;
+	}
 }
+console.log(calculateTotalMortgage)
