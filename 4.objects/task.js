4efa6962b39c7bcb91f86e04dc746613ffc09767
Nassem <Nas7enova@gmail.com>
2026-04-30 22:56:29 +0500

    Четвертое задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+const student1 = new Student("Anna", "female", 21);
+const student2 = new Student("Nick", "male", 20);
+const student3 = new Student("Max", "male", 23);
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.hasOwnProperty("marks"))
+		this.marks.push(...marks);
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.hasOwnProperty("marks") || (this.marks.length === 0)) return 0;
+	const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+	return sum / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
