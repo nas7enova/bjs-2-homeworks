357927ef77be95e743dab10c842626e4552a1b1c
Nassem <Nas7enova@gmail.com>
2026-05-04 00:18:37 +0500

    Добавлено задание со звездочкой

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -90,3 +90,38 @@ class Library {
 		return null;
 	}
 }
+
+class Student {
+	constructor(name) {
+		this.name = name;
+		this.marks = {};
+	}
+
+	addMark(mark, subject) {
+		if (mark >= 2 && mark <= 5) {
+			if (!this.marks[subject]) {
+				this.marks[subject] = [];
+			}
+			this.marks[subject].push(mark);
+		}
+	}
+
+	getAverageBySubject(subject) {
+		if (!this.marks[subject] || this.marks[subject].length === 0) {
+			return 0;
+		}
+		const sum = this.marks[subject].reduce((acc, mark) => acc + mark, 0);
+		return sum / this.marks[subject].length;
+	}
+
+	getAverage() {
+		const subjects = Object.keys(this.marks);
+		if (subjects.length === 0) return 0;
+
+		const sumOfAverages = subjects.reduce((sum, subject) => {
+			return sum + this.getAverageBySubject(subject);
+		}, 0);
+
+		return sumOfAverages / subjects.length;
+	}
+}
